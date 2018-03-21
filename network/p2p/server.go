94e07d95aec491ad45d2bf1d5ab6a16bff3556fb
masterdxy <master.dxy@gmail.com>
2018-03-21 17:27:27 +0800

    fix typo

--- network/p2p/server.go
+++ network/p2p/server.go
@@ -963,7 +963,7 @@ type NodeInfo struct {
 	ID    string `json:"id"`    // Unique node identifier (also the encryption key)
 	Name  string `json:"name"`  // Name of the node, including client type, version, OS, custom data
 	Local string `json:"local"` // Local node type
-	Enode string `json:"enode"` // Enode URL for adding this peer from remote peers
+	Enode string `json:"hnode"` // Enode URL for adding this peer from remote peers
 	IP    string `json:"ip"`    // IP address of the node
 	Ports struct {
 		Discovery int `json:"discovery"` // UDP listening port for discovery protocol
