8f388be433da9714c4f17c66c0b0c8bc8a3f51de
codex8080 <codex8080@163.com>
2018-05-18 16:24:07 +0800

    Master:调整文件目录及结构

--- peermanager/api.go
+++ peermanager/api.go
@@ -40,6 +40,7 @@ import (
 	"github.com/hpb-project/ghpb/common/rlp"
 	"github.com/hpb-project/ghpb/network/rpc"
 	"github.com/hpb-project/ghpb/common/trie"
+	"github.com/hpb-project/ghpb/interface"
 )
 
 const defaultTraceTimeout = 5 * time.Second
@@ -580,3 +581,119 @@ func storageRangeAt(st state.Trie, start []byte, maxResult int) StorageRangeResu
 	}
 	return result
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//copy from bind.go
+
+// ContractBackend implements bind.ContractBackend with direct calls to Hpb
+// internals to support operating on contracts within subprotocols like eth and
+// swarm.
+//
+// Internally this backend uses the already exposed API endpoints of the Hpb
+// object. These should be rewritten to internal Go method calls when the Go API
+// is refactored to support a clean library use.
+type ContractBackend struct {
+	eapi  *hpbapi.PublicHpbAPI        // Wrapper around the Hpb object to access metadata
+	bcapi *hpbapi.PublicBlockChainAPI      // Wrapper around the blockchain to access chain data
+	txapi *hpbapi.PublicTransactionPoolAPI // Wrapper around the transaction pool to access transaction data
+}
+
+// NewContractBackend creates a new native contract backend using an existing
+// Hpb object.
+func NewContractBackend(apiBackend hpbapi.Backend) *ContractBackend {
+	return &ContractBackend{
+		eapi:  hpbapi.NewPublicHpbAPI(apiBackend),
+		bcapi: hpbapi.NewPublicBlockChainAPI(apiBackend),
+		txapi: hpbapi.NewPublicTransactionPoolAPI(apiBackend, new(hpbapi.AddrLocker)),
+	}
+}
+
+// CodeAt retrieves any code associated with the contract from the local API.
+func (b *ContractBackend) CodeAt(ctx context.Context, contract common.Address, blockNum *big.Int) ([]byte, error) {
+	return b.bcapi.GetCode(ctx, contract, toBlockNumber(blockNum))
+}
+
+// CodeAt retrieves any code associated with the contract from the local API.
+func (b *ContractBackend) PendingCodeAt(ctx context.Context, contract common.Address) ([]byte, error) {
+	return b.bcapi.GetCode(ctx, contract, rpc.PendingBlockNumber)
+}
+
+// ContractCall implements bind.ContractCaller executing an Hpb contract
+// call with the specified data as the input. The pending flag requests execution
+// against the pending block, not the stable head of the chain.
+func (b *ContractBackend) CallContract(ctx context.Context, msg hpb_project.CallMsg, blockNum *big.Int) ([]byte, error) {
+	out, err := b.bcapi.Call(ctx, toCallArgs(msg), toBlockNumber(blockNum))
+	return out, err
+}
+
+// ContractCall implements bind.ContractCaller executing an Hpb contract
+// call with the specified data as the input. The pending flag requests execution
+// against the pending block, not the stable head of the chain.
+func (b *ContractBackend) PendingCallContract(ctx context.Context, msg hpb_project.CallMsg) ([]byte, error) {
+	out, err := b.bcapi.Call(ctx, toCallArgs(msg), rpc.PendingBlockNumber)
+	return out, err
+}
+
+func toCallArgs(msg hpb_project.CallMsg) hpbapi.CallArgs {
+	args := hpbapi.CallArgs{
+		To:   msg.To,
+		From: msg.From,
+		Data: msg.Data,
+	}
+	if msg.Gas != nil {
+		args.Gas = hexutil.Big(*msg.Gas)
+	}
+	if msg.GasPrice != nil {
+		args.GasPrice = hexutil.Big(*msg.GasPrice)
+	}
+	if msg.Value != nil {
+		args.Value = hexutil.Big(*msg.Value)
+	}
+	return args
+}
+
+func toBlockNumber(num *big.Int) rpc.BlockNumber {
+	if num == nil {
+		return rpc.LatestBlockNumber
+	}
+	return rpc.BlockNumber(num.Int64())
+}
+
+// PendingAccountNonce implements bind.ContractTransactor retrieving the current
+// pending nonce associated with an account.
+func (b *ContractBackend) PendingNonceAt(ctx context.Context, account common.Address) (nonce uint64, err error) {
+	out, err := b.txapi.GetTransactionCount(ctx, account, rpc.PendingBlockNumber)
+	if out != nil {
+		nonce = uint64(*out)
+	}
+	return nonce, err
+}
+
+// SuggestGasPrice implements bind.ContractTransactor retrieving the currently
+// suggested gas price to allow a timely execution of a transaction.
+func (b *ContractBackend) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
+	return b.eapi.GasPrice(ctx)
+}
+
+// EstimateGasLimit implements bind.ContractTransactor triing to estimate the gas
+// needed to execute a specific transaction based on the current pending state of
+// the backend blockchain. There is no guarantee that this is the true gas limit
+// requirement as other transactions may be added or removed by miners, but it
+// should provide a basis for setting a reasonable default.
+func (b *ContractBackend) EstimateGas(ctx context.Context, msg hpb_project.CallMsg) (*big.Int, error) {
+	out, err := b.bcapi.EstimateGas(ctx, toCallArgs(msg))
+	return out.ToInt(), err
+}
+
+// SendTransaction implements bind.ContractTransactor injects the transaction
+// into the pending pool for execution.
+func (b *ContractBackend) SendTransaction(ctx context.Context, tx *types.Transaction) error {
+	raw, _ := rlp.EncodeToBytes(tx)
+	_, err := b.txapi.SendRawTransaction(ctx, raw)
+	return err
+}
+
+
+
